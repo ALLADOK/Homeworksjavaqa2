dced43b9c08e2e9880fe135cdb1e17b9b36e80a8
ALLA <all.tarasowa808@yandex.ru>
2024-04-23 23:14:48 +0300

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int amout = 5000;
+        int mile = 20;
+
+        int bonus;
+        bonus = amout / mile;
+        System.out.println(bonus);
+        System.out.println("Бонусные мили");
+    }
+}
